632cf01871eab81563465efc75a35d05f10e8217
Etheraex <mladjaether@gmail.com>
2019-06-01 22:03:58 +0200

    Promena Subjects stranice

--- TestIT/TestIT/wwwroot/js/Subjects.js
+++ TestIT/TestIT/wwwroot/js/Subjects.js
@@ -1,5 +1,12 @@
 //AddSubjectsPerYear();
 
+// Po defaultu su sakriveni a ovime i otkrivamo cim se loaduje stranica
+Hide('1');
+Hide('2')
+Hide('3')
+Hide('4')
+
+
 function AddSubjectsPerYear(){
     let div = document.getElementsByClassName("1st-year");
     AppendSubjects(div[0]);
@@ -33,4 +40,33 @@ function GoToSubjectPage(){
     // window.location.href = '/Home/ConcreteSubject/' + this.value;
     
     window.location.href = '/Home/ConcreteSubject/';
+}
+
+function Hide(num){
+    
+    let div;
+    switch(num){
+        case '1':
+            div = document.getElementsByClassName("1st-year");
+            div[0].hidden = !div[0].hidden;
+        break;
+        case '2':
+            div = document.getElementsByClassName("2nd-year");
+            div[0].hidden = !div[0].hidden;
+        break;
+        case '3':
+            div = document.getElementsByClassName("3rd-year");
+            div[0].hidden = !div[0].hidden;
+        break;
+        case '4':
+            div = document.getElementsByClassName("4th-year");
+            div[0].hidden = !div[0].hidden;
+        break;
+    }
+}
+
+
+
+window.HideSubjects = function HideSubjects(num){
+    Hide(num);
 }
