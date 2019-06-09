f296506cd0d94913d597e45fcf50327b683778a8
Etheraex <mladjaether@gmail.com>
2019-06-09 23:56:25 +0200

    updejt Competition i Course stranice

--- TestIT/TestIT/Models/Competition.cs
+++ TestIT/TestIT/Models/Competition.cs
@@ -10,11 +10,17 @@ namespace TestIT.Models
     {
         [Key]
         public int ID { get; set; }
+        [Display(Name="Ucesnici")]
         public IList<Participation> Participations { get; set; }
+        [Display(Name="Kviz")]
         public Quiz Quiz { get; set; }
+        [Display(Name="Naziv")]
         public String Name { get; set; }
+        [Display(Name="Pocetak")]
         public DateTime StartDate { get; set; }
+        [Display(Name="Rok")]
         public DateTime Deadline { get; set; }
+        [Display(Name="Predmet")]
         public Course Course { get; set; }
         public Competition()
         {
