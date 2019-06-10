a11cf3af2bce8059257df73ac5fc57e5c107304e
Stefan <bogosavljevic.1911@gmail.com>
2019-06-10 08:16:31 +0200

    doradjen edit Question, al nije jos gotov

--- TestIT/TestIT/wwwroot/js/EditQuestion.js
+++ TestIT/TestIT/wwwroot/js/EditQuestion.js
@@ -1,11 +1,19 @@
 ﻿console.log("Hello from edit question");
 let question;
+let answerQuantity = "";
+let answerType = "";
+let answerCount;
 let img = document.getElementById("imageQ");
 let cnvs = document.getElementById("myCanvas");
+let tableBody = document.getElementById("tableBody");
 window.startUp = function startUp(data) {
     console.log(data);
     question = data;
+    answerQuantity = question.Answers.length == 1 ? "single": "multy";
+    answerType = question.Answers[0].x1 == null ? "Text" : "Region";
+    answerCount = question.Answers.length;
     draw();
+    showAnswers();
 }
 
 function draw() {
@@ -22,7 +30,7 @@ function draw() {
     let i = 0;
     question.Answers.forEach(a => {
         ctx.beginPath();
-        drawText(a.RightX1 * img.width, a.RightY1 * img.width, "T" + (i + 1), ctx);
+        drawText(a.x1 * img.width, a.y1 * img.height, "T" + (i + 1), ctx);
         ctx.rect(a.x1 * img.width, a.y1 * img.height, (a.x2 * img.width) - (a.x1 * img.width), (a.y2 * img.height) - (a.y1 * img.height));
         i++;
         ctx.stroke();
@@ -35,4 +43,104 @@ function drawText(x, y, text, ctx) {
     ctx.font = "13px Arial";
     ctx.fillText(text, x - 4, y - 4);
     ctx.fillStyle = tempSytyle;
+}
+
+window.onImageClick = function onImageClick() {
+    let x = event.pageX;
+    let y = event.pageY;
+    x = (x - img.offsetLeft) / img.width;
+    y = (y - img.offsetTop) / img.height;
+    let recWidth = 100;
+    let recdepth = 50;
+    addRegionAnswer(x, y, x + (recWidth / img.width), y + (recdepth / img.height));
+    draw();
+}
+
+
+function addRegionAnswer(x1, y1, x2, y2) {
+    let tempAnswer = {};
+    tempAnswer.x1 = x1;
+    tempAnswer.x2 = x2;
+    tempAnswer.y1 = y1;
+    tempAnswer.y2 = y2;
+    tempAnswer.type = answerQuantity + "-" + answerType;
+    if (answerQuantity == "single")
+        question.Answers = [];
+    question.Answers.push(tempAnswer);
+    showAnswers();
+}
+
+function showAnswers() {
+    multyAnswerArea.hidden = "";
+    tableBody.innerHTML = "";
+    let i = 0;
+    for (let i = 0; i < question.Answers.length; i++) {
+        showAnswer(question.Answers[i], i );
+    }
+    draw();
+}
+
+function showAnswer(answer, index) {
+    let tr = document.createElement("tr");
+    let tempLabel = document.createElement("label");
+    let td = document.createElement("td");
+    let infoDiv = document.createElement("div");
+    td.appendChild(infoDiv);
+    tr.appendChild(td);
+    if (answerType.includes("Text")) {
+        tempLabel.innerHTML = answer.answerText;
+        if (answerQuantity == "multy") {
+            let checkBox = document.createElement("input");
+            checkBox.type = "checkbox"
+            checkBox.onclick = function () { togleCorrect(index); };
+            td = document.createElement("td");
+            td.appendChild(checkBox);
+            tr.appendChild(td);
+        }
+    }
+    else if (answerType.includes("Region")) {
+        let widthEditBox = document.createElement("input");
+        widthEditBox.value = (answer.x2 - answer.x1) * img.width;
+        widthEditBox.onchange = function () { areaChange(widthEditBox.value, index, true) }
+        let heightEditBox = document.createElement("input");
+        heightEditBox.value = (answer.x2 - answer.x1) * img.height;
+        heightEditBox.onchange = function () { areaChange(heightEditBox.value, index,false) }
+        tempLabel.innerHTML = "T" + (index + 1); 
+        infoDiv.appendChild(tempLabel);
+        infoDiv.appendChild(widthEditBox);
+        infoDiv.appendChild(heightEditBox);
+    }
+    if (answerQuantity == "multy") {
+        let button = document.createElement("input");
+        button.type = "button";
+        button.value = "x";
+        button.className = "btn btn-danger"
+        button.onclick = function () { removeAnswer(index); };
+        td = document.createElement("td");
+        td.appendChild(button);
+        tr.appendChild(td);
+    }
+    tableBody.appendChild(tr);
+}
+function togleCorrect(index) {
+    question.Answers[index].isCorrect = !QuestionTemplate.Answers[index].isCorrect
+    console.log(QuestionTemplate.Answers[index]);
+}
+
+function removeAnswer(index) {
+    question.Answers.splice(index, 1);
+    showAnswers();
+}
+
+function areaChange(size,index, isWidthEdit) {
+    let answer = question.Answers[index];
+
+    if (isWidthEdit) {
+        size = size / img.width;
+        answer.x2 = question.Answers[index].x1 + size;
+    } else if (!isWidthEdit) {
+        size = size / img.height;
+        answer.y2 = question.Answers[index].y1 + size;
+    }
+    draw();
 }
